e51dec88bb68e0ae79b40425d10f123e9a34536c
Santiago del Rey <santi.delrey@gmail.com>
2025-05-30 16:20:49 +0200

    Finish models for RQ2

---
+++ notebooks/install_packages.R
@@ -0,0 +1,32 @@
+# Install required packages R Markdown notebooks
+
+required_packages <- c(
+  "arrow",
+  "tidyverse",
+  "lme4",
+  "lmerTest",
+  "betareg",
+  "robustbetareg",
+  "gamlss",
+  "glmmTMB",
+  "DHARMa",
+  "broom",
+  "car",
+  "MASS",
+  "performance",
+  "emmeans",
+  "multcomp",
+  "multcompView",
+  "h2o",
+  "gridExtra",
+  "ggplot2",
+  "ggfortify",
+  "ggpubr"
+)
+
+installed_packages <- rownames(installed.packages())
+for (pkg in required_packages) {
+  if (!pkg %in% installed_packages) {
+    install.packages(pkg)
+  }
+}
